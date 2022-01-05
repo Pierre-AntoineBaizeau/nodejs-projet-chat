7c2abe3e38ed7da90d6b046818d5962c7a5350f7
Karen <karen.azoulay@edu.devinci.fr>
2022-01-05 23:15:29 +0100

    test bdd

---
+++ index.js
@@ -0,0 +1,17 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+async function main() {
+    // Connect the client
+    await prisma.$connect()
+    // ... you will write your Prisma Client queries here
+}
+
+main()
+    .catch((e) => {
+        throw e
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
