e807b60e87571d72768fba460600da575eeffa75
Xavi-windows <xavier.lami@edu.devinci.fr>
2022-01-06 20:48:56 +0100

    merge routes -> messagesApi

--- routes/messages.js
+++ routes/messages.js
@@ -1,29 +1,34 @@
-const express = require('express');
-const router = express.Router();
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+var express = require("express");
+var router = express.Router();
 
 /* GET messages listing. */
-router.get('/', function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json({msg: "send messages"});
+router.get("/", async function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  try {
+    await db.message.find();
+    console.log("message find ");
+    return res.json({ msg: "Succes" });
+  } catch (e) {
+    console.log(e);
+    res.status(403).json({ msg: "404 error" });
+  }
 });
 
 /* Post  message. */
-router.post('/',  async function(req, res, next) {
-    try {
-      await prisma.message.create({
-        data: {
-           content: 'testt',
-           authorId: 1
-        },
-      })
-      console.log('Created messages');
-      return res.json({msg: "message create"});
-    } catch(e) {
-      console.error(e);
-      return res.status(403).json({msg: 'something went wrong'});
-    }
+router.post("/", async function (req, res, next) {
+  try {
+    await prisma.message.create({
+      data: {
+        content: "testt",
+        authorId: 1,
+      },
+    });
+    console.log("Created messages");
+    return res.json({ msg: "message create" });
+  } catch (e) {
+    console.error(e);
+    return res.status(403).json({ msg: "something went wrong" });
+  }
 });
 
 module.exports = router;
