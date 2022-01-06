d13cff553a23ed59fe5873f0d44e11b0c613a2c0
BenjaminBurstein <benjamin.burstein@edu.devinci.fr>
2022-01-06 14:56:45 +0100

    add new route

--- routes/messages.js
+++ routes/messages.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+
+app.use('/messages', router);
 
 /* GET messages listing. */
 router.get('/', function(req, res, next) {
-  res.send('Ressource message');
+  res.status(200).json({msg: "get messages"});
+});
+
+/* Post  message. */
+router.post('/', function(req, res, next) {
+  res.status(201).json({msg: "send messages"});
 });
 
 module.exports = router;
