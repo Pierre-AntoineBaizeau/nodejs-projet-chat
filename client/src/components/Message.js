029662510a455e165bebb5dbbff562c96691d314
BenjaminBurstein <benjamin.burstein@edu.devinci.fr>
2022-01-07 10:43:24 +0100

    display messages

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,28 +1,46 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../style/Message.css'
 
 const Message = () => {
     const [myMessage, setMyMessage] = useState(false)
+    const [messages, setMessages] = useState([])
 
+    useEffect(   () => {
+       fetch('http://localhost:3000/messages')
+            .then((response) => response.json())
+            .then((data) => {
+                setMessages(data)
+            })
+
+    }, [])
     return (
-        <div className={myMessage ? "blocMyMessage" : ""}>
-            {
-                myMessage ?
-                    (
-                        <div>
-                            <p>Moi le 21/07</p>
-                            <span className="myMessage message">Mon message</span>
+       <>
+           { messages.length > 0 && (
+                messages.map((message,index) => {
+                    return (
+                        <div key={index} className={myMessage ? "blocMyMessage" : ""}>
+                            {
+                                myMessage ?
+                                    (
+                                        <div>
+                                            <p>Moi le 21/07</p>
+                                            <span className="myMessage message">{message.content}</span>
+                                        </div>
+                                    ) : (
+                                        <>
+                                            <p>Julia le 21/07</p>
+                                            <p className="message">
+                                                <span>{message.content}</span>
+                                            </p>
+                                        </>
+                                    )
+                            }
                         </div>
-                    ) : (
-                        <>
-                            <p>Julia le 21/07</p>
-                            <p className="message">
-                                <span>Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic. Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.</span>
-                            </p>
-                        </>
                     )
-            }
-        </div>
+                })
+            )}
+
+       </>
     );
 }
 
