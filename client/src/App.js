82b6aa3377cb823956a8f5b23c05f00c27733495
BenjaminBurstein <benjamin.burstein@edu.devinci.fr>
2022-01-07 09:48:09 +0100

    router frotn

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Login from './components/Login';
 import Chat from './components/Chat';
 
 const App = () => (
-    <BrowserRouter>
-      <Route path="/" exact component={Login} />
-      <Route path="/chat" component={Chat} />
-    </BrowserRouter>
+    <Router>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">login</Link>
+                    </li>
+                    <li>
+                        <Link to="/chat">chat</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route exact path="/" element={<Login/>}/>
+                <Route exact path="/chat" element={<Chat/>}/>
+            </Routes>
+        </div>
+    </Router>
 );
 
 export default App;
