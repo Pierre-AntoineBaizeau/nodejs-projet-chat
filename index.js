9fda19ac1292510dab0e633e9c4a95a8b63132b7
AbdelTRS <abdeltrs@hotmail.com>
2022-01-07 15:42:21 +0100

    Fixbug

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const { PrismaClient } = require('@prisma/client')
-const express = require('express');
-require('@prisma/client');
-const app = express();
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const multer = require('multer');
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+
 const prisma = new PrismaClient()
 
 async function main() {
@@ -15,14 +8,6 @@ async function main() {
     // ... you will write your Prisma Client queries here
 }
 
-const route = require('./routes');
-app.use('/', route);
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
-});
-
 main()
     .catch((e) => {
         throw e
@@ -30,4 +15,3 @@ main()
     .finally(async () => {
         await prisma.$disconnect()
     })
-
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 var express = require("express");
 var router = express.Router();
-const auth = require('./auth');
-const createError = require('http-errors')
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -9,15 +7,4 @@ router.get("/", function (req, res, next) {
   res.status(200).json({ msg: "hello" });
 });
 
-router.use('/auth', auth);
-router.use(async (req, res, next) => {
-  next(createError.NotFound('Route not Found'))
-})
-
-router.use( (err, req, res, next) => {
-  res.status(err.status || 500).json({
-      status: false,
-      message: err.message
-  })
-})
 module.exports = router;
